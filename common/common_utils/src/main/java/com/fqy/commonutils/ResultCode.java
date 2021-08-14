7f170881f4382a54e0379a7645ec4eb27471429d
jane_glb <jane_glb@aliyun.com>
2021-08-15 01:58:33 +0800

    2th version fqy_2021.8.15

---
+++ common/common_utils/src/main/java/com/fqy/commonutils/ResultCode.java
@@ -0,0 +1,12 @@
+package com.fqy.commonutils;
+
+/**
+ * @author fan_jennifer
+ * @create 2021-08-2021/8/14 14:00
+ */
+public interface ResultCode {
+    public static Integer SUCESS=20000;//成功
+    public static Integer ERROR=20001;//失败
+
+
+}
--- service/service_edu/src/main/java/com/fqy/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/fqy/eduservice/controller/EduTeacherController.java
@@ -1,12 +1,17 @@
 package com.fqy.eduservice.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.fqy.commonutils.R;
 import com.fqy.eduservice.entity.EduTeacher;
+import com.fqy.eduservice.entity.vo.TeacherQuery;
 import com.fqy.eduservice.service.EduTeacherService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,18 +35,77 @@ public class EduTeacherController {
     //查询讲师表所有数据
     @ApiOperation(value = "所有讲师列表")
     @GetMapping("/All")
-    public List<EduTeacher> findAllTeacher() {
+    public R findAllTeacher() {
         List<EduTeacher> list = teacherService.list(null);
-        return list;
+        return R.ok().data("items", list);
 
 
     }
 
     @DeleteMapping("{id}")
     @ApiOperation(value = "逻辑删除讲师")
-    public boolean removeTeacher(@ApiParam(name = "id", value = "讲师ID", required = true) @PathVariable String id) {
+    public R removeTeacher(@ApiParam(name = "id", value = "讲师ID", required = true) @PathVariable String id) {
         boolean flag = teacherService.removeById(id);
-        return flag;
+        if (flag) {
+            return R.ok();
+        } else {
+            return R.erro();
+        }
     }
-}
+
+    @GetMapping("/pageTeacher/{current}/{limit}")
+    public R pageListTeacher(@PathVariable long current,
+                             @PathVariable long limit) {
+        Page<EduTeacher> pageTeacher = new Page<>(current, limit);
+
+        teacherService.page(pageTeacher, null);
+
+        long total = pageTeacher.getTotal();
+        List<EduTeacher> records = pageTeacher.getRecords();
+
+        return R.ok().data("total", total).data("rows", records);
+
+
+    }
+
+    @GetMapping("pageTeacherCondition/{current}/{limit}")
+    public R pageTeacherCondition(@PathVariable long current, @PathVariable long limit,
+                                  TeacherQuery teacherQuery) {
+        //创建page对象
+        Page<EduTeacher> pageTeacher = new Page<>(current, limit);
+
+        //构件条件
+        QueryWrapper<EduTeacher> wrapper = new QueryWrapper<>();
+        String name = teacherQuery.getName();
+        Integer level = teacherQuery.getLevel();
+        String begin = teacherQuery.getBegin();
+        String end = teacherQuery.getEnd();
+        if (!StringUtils.isEmpty(name)){
+            wrapper.like("name",name);
+        }
+        if (!StringUtils.isEmpty(level)){
+            wrapper.eq("level",level);
+        }
+        if (!StringUtils.isEmpty(begin)){
+            wrapper.ge("gmt_create",begin);
+        }
+        if (!StringUtils.isEmpty(end)){
+            wrapper.le("gmt_create",end);
+        }
+
+
+
+        //调用方法实现条件查询分页
+        teacherService.page(pageTeacher,wrapper);
+
+        long total = pageTeacher.getTotal();
+        List<EduTeacher> records = pageTeacher.getRecords();
+
+        return R.ok().data("total", total).data("rows", records);
+
+
+    }
+
+    }
+
 
